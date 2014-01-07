8abca9a2d2e5a43162cdb2facdd8c8ca25252b18
Khurrum Chaudhry <khurrum.mehmood@gmail.com>
2014-01-07 01:59:42 -0500

    added location detection
    has memory leak

--- FollowMe/src/com/khurrum/followme/Mylocation.java
+++ FollowMe/src/com/khurrum/followme/Mylocation.java
@@ -1,26 +1,74 @@
 package com.khurrum.followme;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+public class Mylocation extends FragmentActivity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener, LocationListener {
+	LocationRequest lr;
+	LocationClient lc;
+	SupportMapFragment fragment;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mylocation);
+		fragment = new SupportMapFragment();
+		getSupportFragmentManager().beginTransaction()
+				.add(R.id.fragCont, fragment).commit();
 
-public class Mylocation extends FragmentActivity {
+		lr = LocationRequest.create();
+		lr.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		lc = new LocationClient(this.getApplicationContext(), this, this);
+		lc.connect();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mylocation);
-    }
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		// getMenuInflater().inflate(R.menu.mylocation, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.mylocation, menu);
-        return true;
-    }
-    
+	@Override
+	public void onLocationChanged(Location location) {
+		CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+				new LatLng(location.getLatitude(), location.getLongitude()), 15);
+		fragment.getMap().animateCamera(cameraUpdate);
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
